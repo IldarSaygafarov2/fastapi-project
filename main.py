a828d17ce49ea61710327e19d80cea5612964988
skrill <tmkz@gmail.com>
2025-08-12 17:04:56 +0500

    initialized project

---
+++ main.py
@@ -0,0 +1,15 @@
+# fastapi-project
+
+# main, master
+# pull request
+
+# exit()
+
+# создать виртуальное окружение и сразу активировать
+# python -m venv venv && venv\Scripts\activate
+
+
+# pip install "fastapi[standard]"
+# pip install fastapi
+# pip freeze - показывает список библиотек и их версий
+
